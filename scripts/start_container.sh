003c5086ef301b01269645facb1cd06222735f12
Surya161002 <127496011+Surya161002@users.noreply.github.com>
2024-09-08 23:29:22 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull suryag16/simple-python-flask-app
 # Run the Docker image as a container
-docker run -d -p 8000:8000 suryag16/simple-python-flask-app 
+docker run -d -p 8000:8000 suryag16/simple-python-flask-app
